b54a227fdfe00f42f5bee5357b6ba493f440cfe4
Srinivas Bodduru <srinivas@hmgtechnology.com>
2026-07-29 16:20:47 +0530

    update attachemtn tabel schema

--- EDI_Tebra_schema.sql
+++ EDI_Tebra_schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE "EDI_Tebra".attachments (
     rotation_degrees int4 NULL,
     upload_source text NULL,
     associated_claim_dcn varchar(100) NULL,
-    claimcreationresposne jsonb NULL,
+    claim_creation_response jsonb NULL,
     CONSTRAINT claim_attachments_pkey PRIMARY KEY (id),
     CONSTRAINT attachments_assigned_to_id_fkey FOREIGN KEY (assigned_to_id) REFERENCES rcm.users(id),
     CONSTRAINT attachments_client_id_fkey FOREIGN KEY (client_id) REFERENCES "EDI_Tebra".client(client_id) ON DELETE CASCADE,
